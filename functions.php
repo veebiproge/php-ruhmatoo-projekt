c93a2fac89b99176df812b542d7a90b4c69b781c
case112 <case112@users.noreply.github.com>
2016-11-23 10:58:36 +0200

    added some files

---
+++ functions.php
@@ -0,0 +1,64 @@
+<?php
+
+	require("/home/nikopeos/config.php");
+	
+	$database = "if16_case112";
+	$mysqli = new mysqli($serverHost, $serverUsername, $serverPassword, $database);
+	
+	//require("../class/User.class.php");
+	//$User = new User($mysqli);
+	
+	//require("../class/Helper.class.php");
+	//$Helper = new Helper($mysqli);
+	
+	
+	// functions.php
+	//var_dump($GLOBALS);
+	
+	// see fail, peab olema kõigil lehtedel kus 
+	// tahan kasutada SESSION muutujat
+	session_start();
+	
+	//***************
+	//**** SIGNUP ***
+	//***************
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*function sum($x, $y) {
+		
+		return $x + $y;
+		
+	}
+	
+	
+	function hello($firsname, $lastname) {
+		
+		return "Tere tulemast ".$firsname." ".$lastname."!";
+		
+	}
+	
+	echo sum(5123123,123123123);
+	echo "<br>";
+	echo hello("Romil", "Robtsenkov");
+	echo "<br>";
+	echo hello("Juku", "Juurikas");
+	*/
+
+?>
