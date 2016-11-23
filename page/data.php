e89408fb79d470b4a173cee1bdb969aac8fd358e
randotm <randotm@users.noreply.github.com>
2016-11-23 11:12:25 +0200

    Started data.php

---
+++ page/data.php
@@ -0,0 +1,6 @@
+<?php
+	
+?>
+<?php require("../partials/header.php"); ?>
+Otsing: <input type = "text" name = "search"> <input type = "submit" value = "Otsi">
+<?php require("../partials/footer.php"); ?>
