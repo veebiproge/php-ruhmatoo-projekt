4a73acdc9e019c493648dd6cd509ea3a6452381f
randotm <randotm@users.noreply.github.com>
2016-12-07 10:52:47 +0200

    Updated readme.

---
+++ partials/loggedInHeader.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<?php
+	
+?>
+<html>
+<head>
+	<title>Pealkiri</title>
+	
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>	
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	
+</head>
+<body>
+
+<nav class="navbar navbar-default">
+	<ul class="nav navbar-nav navbar-right">
+		<li role="presentation"><a href="signup.php">Lisa kodugrupp</a></li>
+		<li role="presentation"><a href="#tere">Lisa kursus</a></li>
+		<li role="presentation"><a href="#">Lisa tööharu</a></li>
+		<li role="presentation"><a href="?logout=1">Logi välja</a></li>
+	</ul>
+</nav>
