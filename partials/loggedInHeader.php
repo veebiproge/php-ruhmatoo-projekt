71bf8b20770b9162030c66ff05b803b5d76f56ad
randotm <randotm@users.noreply.github.com>
2016-12-10 15:44:18 +0200

    Updated loggedInHeader.php.

--- partials/loggedInHeader.php
+++ partials/loggedInHeader.php
@@ -22,9 +22,10 @@
 
 <nav class="navbar navbar-default">
 	<ul class="nav navbar-nav navbar-right">
-		<li role="presentation"><a href="signup.php">Lisa kodugrupp</a></li>
-		<li role="presentation"><a href="#tere">Lisa kursus</a></li>
-		<li role="presentation"><a href="#">Lisa tööharu</a></li>
+		<li role="presentation"><a href="data.php">Administreerimine</a></li>
+		<li role="presentation"><a href="smallgroups.php">Kodugrupid</a></li>
+		<li role="presentation"><a href="courses.php">Kursused</a></li>
+		<li role="presentation"><a href="lines_of_work.php">Tööharud</a></li>
 		<li role="presentation"><a href="?logout=1">Logi välja</a></li>
 	</ul>
 </nav>
