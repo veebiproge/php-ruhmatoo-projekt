f53b2c1bda6ebe077e4817889037bae7700fabaa
randotm <randotm@users.noreply.github.com>
2016-12-10 14:38:29 +0200

    Changed title of pages.

--- partials/header.php
+++ partials/header.php
@@ -4,7 +4,7 @@
 ?>
 <html>
 <head>
-	<title>Pealkiri</title>
+	<title>CDB</title>
 	
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
