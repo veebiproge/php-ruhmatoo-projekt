b2fe8320b2b71d16dbd8829802c4a69ed2a4c953
randotm <randotm@users.noreply.github.com>
2016-12-10 15:45:17 +0200

    Removed extralines from functions.php.

--- functions.php
+++ functions.php
@@ -5,56 +5,6 @@
 	$database = "if16_case112";
 	$link = new mysqli($serverHost, $serverUsername, $serverPassword, $database);
 	
-	
-	
-	
-	// functions.php
-	//var_dump($GLOBALS);
-	
-	// see fail, peab olema kõigil lehtedel kus 
-	// tahan kasutada SESSION muutujat
 	session_start();
-	
-	//***************
-	//**** SIGNUP ***
-	//***************
-	
-	
-	
-	
-	
-	
-	
-	
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	/*function sum($x, $y) {
-		
-		return $x + $y;
-		
-	}
-	
-	
-	function hello($firsname, $lastname) {
-		
-		return "Tere tulemast ".$firsname." ".$lastname."!";
-		
-	}
-	
-	echo sum(5123123,123123123);
-	echo "<br>";
-	echo hello("Romil", "Robtsenkov");
-	echo "<br>";
-	echo hello("Juku", "Juurikas");
-	*/
 
 ?>
